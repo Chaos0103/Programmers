1c1801909955a2be1b4c22b14857ac2243c81ae1
Dong82 <85945540+Chaos0103@users.noreply.github.com>
2022-05-05 22:33:37 +0900

    튜플

---
+++ code/kakao/2.py
@@ -0,0 +1,13 @@
+def solution(s):
+    answer = []
+    s = s[2:len(s) - 2]
+    s = s.split('},{')
+    for i in range(len(s)):
+        s[i] = s[i].split(',')
+    s.sort(key=lambda x: len(x))
+    for part in s:
+        for num in part:
+            if int(num) not in answer:
+                answer.append(int(num))
+    return answer
+  
